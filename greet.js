38edf33c9b5297b154123eadda8a081b0ecd396e
sp010412 <smajwede7@gmail.com>
2021-05-07 17:30:10 +0200

    complete

--- greet.js
+++ greet.js
@@ -3,8 +3,12 @@ var textNameElem = document.querySelector(".textName")
 //get reference to all radio buttons
 var checkedRadioBtn = document.querySelector('.selectTypeRadio1')
 var textNameElem = document.querySelector('.textName')
+
 //get reference to greet button
 var greetMeBtnElem = document.querySelector('.greetMeBtn')
+//get reference to reset button
+var resetBtnElem = document.querySelector('.resetBtn')
+
 //get reference to counter number
 var counterElem = document.querySelector('.counter')
 //get reference to message desplay
@@ -16,8 +20,16 @@ var displayedNameElem = document.querySelector('.displayedName')
 //add an event listener for when the 'greetMessage' button is pressed
 //greetMeBtnElem.addEventListener('click', greetMessage3);
 //retrive names from local storage
+//let key = greetInsta.getNames();
+//localStorage.getItem(JSON.parse(key));
+//console.log(localStorage.getItem('name').length)
+var local = localStorage.getItem('name');
+var names= JSON.parse(local)
+//console.log(names.length)
+//counterElem.innerHTML = names.length
 
-var greetInsta = tools()
+var greetInsta = tools(names)
+counterElem.innerHTML = greetInsta.counter();
 
 function thebuttons1() {
     var checkedRadio = document.querySelector("input[name='selectTypeRadio']:checked");
@@ -34,23 +46,21 @@ function thebuttons1() {
     } else {
         greetInsta.storeNames(textNameElem.value)
         displayedNameElem.innerHTML = greetInsta.greet(checkedRadio.value, textNameElem.value);
-        counterElem.innerHTML = greetInsta.counter()
+        // counterElem.innerHTML = greetInsta.counter()
+        counterElem.innerHTML = greetInsta.counter();
+
+
         //set names  into localstorage.
-    let key = greetInsta.getNames();
-   // let value = greetInsta.counter();
-    localStorage.setItem('name', key);
-    let myItem = localStorage.getItem(key);
+        let key = greetInsta.getNames();
+        // let value = greetInsta.counter();
+        localStorage.setItem('name', JSON.stringify(key));
     }
 }
 greetMeBtnElem.addEventListener('click', thebuttons1);
 
 
-console.log(localStorage)
-// greetMeBtnElem.addEventListener('click', function () {
-//     greetInsta.storeNames(nameList)
-//     nameList++;
-//     //store how many times a cat was spotted.
-//     localStorage['spotted'] = nameList;
-    
+// function duplicate() {
+//     if (textNameElem.value > 2) {
+//         return greetInsta.counter()--
+//     }
 // }
-// );
--- greetingsFactory.js
+++ greetingsFactory.js
@@ -5,11 +5,16 @@ function tools(existingNames) {
     var nameList = existingNames || [];
 
     function storeNames(name) {
-        nameList.push(name)
+        name = name.charAt(0).toUpperCase() + name.slice(1)
+        if (!nameList.includes(name)) {
+            return nameList.push(name)
+        } else {
+            return
+        }
     }
 
     function greet(selectL, name) {
-
+        name = name.charAt(0).toUpperCase() + name.slice(1)
         if (selectL === "English") {
             return "Hi, " + name;
         }
@@ -26,15 +31,26 @@ function tools(existingNames) {
         return nameList.length
     }
 
-    function getNames(){
+    function getNames() {
         return nameList
     }
+    function backZero(name) {
+        if (nameList[name] === undefined) {
+            greetingsCounter++;
+            //add an entry for the user that was greeted in the Object Map
+            namesGreeted[userName] = 0;
+            //update the DOM to display the counter
+            greetingsElem.innerHTML = greetingsCounter;
+        }
+    }
+
 
     return {
         greet,
         counter,
         storeNames,
-        getNames
+        getNames,
+
     }
 
 }
